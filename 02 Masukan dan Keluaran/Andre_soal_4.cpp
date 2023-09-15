830ce15beb59a44f14da0857fef3f0b74dd6b570
simanjuntak12 <simanjuntaka318@gmail.com>
2023-09-15 08:03:26 +0700

    Tugas 2 DDP

---
+++ 02 Masukan dan Keluaran/Andre_soal_4.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    float a, b;
+    cin >> a >> b ;
+    
+    cout << "Hasil dari Penjumlahan " << a << " dengan " << b << " adalah " << a + b << endl;
+    cout << "Hasil dari Pengurangan " << a << " dengan " << b << " adalah " << a - b << endl;
+    cout << "Hasil dari Perkalian " << a << " dengan " << b << " adalah " << a * b << endl;
+    cout << "Hasil dari Pembagian " << a << " dengan " << b << " adalah " << a / b << endl;
+    return 0;
+}
---
+++ 02 Masukan dan Keluaran/Andre_soal_6.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+int a, b;
+    cin >> a >> b;
+    
+    int pembagian = a / b;
+    int sisa_bagi = a % b;
+    
+    cout << "masing-masing " <<pembagian<< endl;
+    cout << "bersisa "<<sisa_bagi<< endl;
+
+    return 0;
+}
